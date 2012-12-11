0b14b6249d5c83c0aec681a51eb81d2b983e1aff
Artem Ponomarev <garorobe@gmail.com>
2012-12-11 21:12:24 +1100

    Test View

--- directory/views.py
+++ directory/views.py
@@ -1,1 +1,12 @@
 # Create your views here.
+from django.http import HttpResponse
+from directory.models import *
+
+
+def categories(request):
+    rootNodes = Node.objects.filter(parent_id = 1)
+    t = loader.get_template('frontend/main.html')
+    c = Context({
+        'rootNodes': rootNodes,
+    })
+    return HttpResponse(t.render(c))
